ac91ab492b87757bcbbd7dbdba17ec69a9631b95
root <root@localhost.localdomain>
2016-12-30 20:04:51 -0500

    fixed event registration because it was dumb and bad and wrong

--- TotemHelper.lua
+++ TotemHelper.lua
@@ -1,6 +1,6 @@
 local Buff_EventFrame = CreateFrame("Frame")
-Buff_EventFrame:RegisterEvent("")
-Buff_EventFrame:SetScript("UNIT_AURA", "player",
+Buff_EventFrame:RegisterEvent("UNIT_AURA", "player")
+Buff_EventFrame:SetScript("OnEvent",
     function(self, event, ...)
         local arg1
         print("Buffs Are Active on " .. arg1)
