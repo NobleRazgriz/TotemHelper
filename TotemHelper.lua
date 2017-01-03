9814cb754540bd40da577239eb453e730bc49c41
root <root@localhost.localdomain>
2017-01-03 17:16:47 -0500

    basic helloworld message

--- TotemHelper.lua
+++ TotemHelper.lua
@@ -1,8 +1,1 @@
-local Buff_EventFrame = CreateFrame("Frame")
-Buff_EventFrame:RegisterEvent("UNIT_AURA", "player")
-Buff_EventFrame:SetScript("OnEvent",
-    function(self, event, ...)
-        local arg1
-        print("Buffs Are Active on " .. arg1)
-    end
-)
+message("Hello World")
