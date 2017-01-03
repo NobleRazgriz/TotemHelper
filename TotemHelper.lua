7317840b78ce8e425d2c92299b5ad62071520af4
root <root@localhost.localdomain>
2017-01-03 17:55:51 -0500

    now prints holla on reload ui or load or whatever

--- TotemHelper.lua
+++ TotemHelper.lua
@@ -1,1 +1,16 @@
-message("Hello World")
+-- message("Hello World")
+
+--local Congrats_EventFrame = CreateFrame("Frame")
+--Congrats_EventFrame:RegisterEvent("PLAYER_LEVEL_UP")
+--Congrats_EventFrame:SetScript("OnEvent",
+--	function(self, event, ...)
+--		local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
+--		print('Congratulations on reaching level ' .. arg1 .. ', ' .. UnitName("Player") .. '! You gained ' .. arg2 .. ' HP and ' .. arg3 .. ' MP!')
+--	end)
+
+local TotemFrame = CreateFrame("Frame")
+TotemFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+TotemFrame:SetScript("OnEvent",
+    function(self, event)
+        print ("holla");
+    end)
