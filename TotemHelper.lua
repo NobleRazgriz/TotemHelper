499e179a44958eb340584aef40ed3a0580fa79ee
NobleRazgriz <jaytelles24@gmail.com>
2017-01-08 16:07:28 -0500

    added notes for events and when they fire

--- TotemHelper.lua
+++ TotemHelper.lua
@@ -1,1 +1,44 @@
-message("Hello World")
+-- message("Hello World")
+
+--local Congrats_EventFrame = CreateFrame("Frame")
+--Congrats_EventFrame:RegisterEvent("PLAYER_LEVEL_UP")
+--Congrats_EventFrame:SetScript("OnEvent",
+--	function(self, event, ...)
+--		local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
+--		print('Congratulations on reaching level ' .. arg1 .. ', ' .. UnitName("Player") .. '! You gained ' .. arg2 .. ' HP and ' .. arg3 .. ' MP!')
+--	end)
+
+--local TotemFrame = CreateFrame("Frame")
+--TotemFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+--TotemFrame:SetScript("OnEvent",
+--    function(self, event, ...)
+--        local arg1 = ...
+--        print ("holla");
+--    end)
+
+-- Fires on literally any buff change at all
+-- i.e. ele totems, ghost wolf, walking away from ele totems, etc.
+--local TotemFrame = CreateFrame("Frame")
+--TotemFrame:RegisterEvent("UNIT_AURA")
+--TotemFrame:SetScript("OnEvent",
+--    function(self, event, ...)
+--        local arg1 = ...
+--        print ("holla");
+--    end)
+    
+
+-- local TotemFrame = CreateFrame("Frame")
+-- TotemFrame:RegisterEvent("PLAYER_TOTEM_UPDATE")
+-- TotemFrame:SetScript("OnEvent",
+--     function(self, event, ...)
+--         local arg1 = ...
+--         print ("holla");
+--     end)
+
+-- local TotemFrame = CreateFrame("Frame")
+TotemFrame:RegisterEvent("UNIT_SHAPESHIFT_FORM")
+TotemFrame:SetScript("OnEvent",
+    function(self, event, ...)
+        local arg1 = ...
+        print ("holla");
+    end)
